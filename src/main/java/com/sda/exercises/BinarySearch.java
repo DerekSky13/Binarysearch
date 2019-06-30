22eaba993d578faebd3baef44af59df27083c630
Dariusz Skirzynski <myjobds@gmail.com>
2019-06-30 11:24:42 +0200

    Implementacja kodu

---
+++ src/main/java/com/sda/exercises/BinarySearch.java
@@ -0,0 +1,27 @@
+package com.sda.exercises;
+
+import java.util.List;
+
+public class BinarySearch {
+
+    public int search(List<Integer> list, Integer searched) {
+        return searchBinary(list,0,list.size() -1, searched);
+    }
+
+    public int searchBinary(
+            List<Integer> list,
+            int left,
+            int right,
+            int searched
+    ){
+        if (right>=left) {
+            int mid = left + (right - left)/2;
+            if (list.get(mid) == searched)
+                return mid;
+            if (list.get(mid)> searched)
+                return searchBinary(list, left, mid - 1, searched);
+                return searchBinary(list, mid +1, right, searched);
+        }
+        return -1;
+    }
+}
